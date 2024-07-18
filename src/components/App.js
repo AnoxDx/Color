c2d9be70547502ad5293855112b657ac6feff2c7
LeGeNd <128361863+AnoxDx@users.noreply.github.com>
2024-07-18 17:51:50 +0530

    Create App.js

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Login from './components/Login';
+import Register from './components/Register';
+import Betting from './components/Betting';
+
+const App = () => {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/betting" element={<Betting />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
